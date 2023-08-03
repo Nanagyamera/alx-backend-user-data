d82ab7c911e7c0f1ffdacf38f0f625ab1e4ab458
Nanagyamera <nanagyamera3@gmail.com>
2023-08-03 08:26:55 -0700

    Implement file with PII filtering

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""
+A module for filtering logs.
+"""
+import logging
+import os
+import mysql.connector
+import re
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        def filter_datum(match):
+            return self.REDACTION
+
+        record.msg = re.sub(
+            rf'({"|".join(self.fields)})=([^{self.SEPARATOR}]*)',
+            filter_datum,
+            record.msg
+        )
+        return super().format(record)
+
+
+def get_logger() -> logging.Logger:
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    dbname = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    connection = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=dbname
+    )
+    return connection
+
+
+def main():
+    logger = get_logger()
+    db = get_db()
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users")
+
+    logger.info("Filtered data:")
+    for row in cursor.fetchall():
+        logger.info("; ".join(f"{f}={v}" for f, v in zip(cursor.column_names, row)))
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
