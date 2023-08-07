e5d09c63c8c8ff68659bee74aa380a31fd3fe5fe
Nanagyamera <nanagyamera3@gmail.com>
2023-08-07 14:02:19 -0700

    Implement file with PII filtering

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -40,16 +40,16 @@ def get_logger() -> logging.Logger:
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """Creates a connector to a database.
     """
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
-    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    dbname = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
     connection = mysql.connector.connect(
-        host=db_host,
+        host=host,
         port=3306,
-        user=db_user,
-        password=db_pwd,
-        database=db_name,
+        user=username,
+        password=password,
+        database=dbname,
     )
     return connection
 
