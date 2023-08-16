7ab6e3b51d4c7fa6ed87765466add66bce0a134f
Nanagyamera <nanagyamera3@gmail.com>
2023-08-16 09:17:50 -0700

    Defined SQLAlchemy User model and implemented database operations

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -60,11 +60,17 @@ class DB:
     def update_user(self, user_id: int, **kwargs) -> None:
         """Updates a user based on a given id.
         """
-        user_to_update = self.find_user_by(id=user_id)
-
-        for attr_name, value in kwargs.items():
-            if hasattr(User, attr_name):
-                setattr(user_to_update, attr_name, value)
+        user = self.find_user_by(id=user_id)
+        if user is None:
+            return
+        update_source = {}
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                update_source[getattr(User, key)] = value
             else:
-                raise ValueError(f"'{attr_name}' is not a valid user attribute")
+                raise ValueError()
+        self._session.query(User).filter(User.id == user_id).update(
+            update_source,
+            synchronize_session=False,
+        )
         self._session.commit()
